e8556fdfd86b59640fc18b01c507e0ef7b5ecf16
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2019-10-12 15:50:04 -0400

    Add files via upload

---
+++ Lesson.java
@@ -0,0 +1,94 @@
+package accuracycontroller;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Lesson {
+
+	ArrayList<String> sentences = new ArrayList<String>();
+	ArrayList<String> translations = new ArrayList<String>();
+	ArrayList<String> missedWords = new ArrayList<String>();
+	String name = "";
+	boolean isSentence;
+	int toRemove;
+	
+	public Lesson(ArrayList<String> sentences, ArrayList<String> translations, ArrayList<String> missedWords, String name)
+	{
+		this.sentences = sentences;
+		this.translations = translations;
+		this.missedWords = missedWords;	
+		this.name = name;
+	}
+	
+	public Lesson(ArrayList<String> sentences, ArrayList<String> translations, String name)
+	{
+		this.sentences = sentences;
+		this.translations = translations;
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String pickSentence()
+	{
+		String sentence = "";
+		//creates random integer from size of both arrays
+		Random r = new Random();
+		
+		//if smaller, goes to sentence first then missed words
+		int sToChoose = r.nextInt(sentences.size() + missedWords.size());
+		if(sToChoose < sentences.size())
+		{
+			sentence = sentences.get(sToChoose);
+			toRemove = sToChoose;
+			isSentence = true;
+		}
+		else
+		{
+			sentence = missedWords.get(sToChoose - sentences.size());
+			toRemove = sToChoose - sentences.size();
+			isSentence = false;
+		}
+		
+		return sentence;
+	}
+		
+	public void checkAccuracy(String s, String input)
+	{
+		AccuracyController a1 = new AccuracyController(sentences, missedWords);
+		a1.checkAccuracy(s, input);
+	}
+	
+	public void evaluateWord()
+	{
+		String sentence = this.pickSentence();
+		String input = "";
+		String translation = "";
+		
+		System.out.println("Phrase: " + sentence);
+		
+		//checks if translation needs to be displayed
+		if(isSentence)
+		{
+			translation = translations.get(toRemove);
+			System.out.println("Translation: " + translation);
+		}
+		
+		//checks accuracy, displays result within method
+		this.checkAccuracy(sentence, input);	
+		
+		//removes items
+		if(isSentence)
+		{
+			sentences.remove(toRemove);
+			translations.remove(toRemove);
+		}
+		else
+		{
+			missedWords.remove(toRemove);
+		}
+	}
+}
---
+++ LessonListES.java
@@ -0,0 +1,34 @@
+package accuracycontroller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LessonListES {
+
+	ArrayList<String> ES_sent1 = new ArrayList<String>(Arrays.asList(
+			"Ella come.", "Él bebe.", "Ella bebe.","Él come.", "Yo corro.", "Él corre.", "Nosotros comemos.", "Nosotros corremos."));
+	ArrayList<String> ES_trans1 = new ArrayList<String>(Arrays.asList(
+			"She eats.", "He drinks.", "She drinks.","He eats.", "I run.", "He runs.", "We eat.", "We run."));
+	
+	Lesson ES1 = new Lesson(ES_sent1, ES_trans1, "ES1");
+	
+	ArrayList<String> ES_sent2 = new ArrayList<String>(Arrays.asList(
+			"¡Hola!", "¿Cómo estás?", "Estoy bien.", "Gracias.", "De nada."));
+	ArrayList<String> ES_trans2 = new ArrayList<String>(Arrays.asList(
+			"Hello!", "How are you?", "I'm well.","Thanks.", "You're welcome."));
+	
+	Lesson ES2 = new Lesson(ES_sent2, ES_trans2, "ES2");
+		
+	ArrayList<Lesson> ESList = new ArrayList<Lesson>(Arrays.asList(ES1, ES2));
+	public Lesson getLesson(String s)
+	{
+        for (int i = 0; i < ESList.size(); i++)
+        {
+        	if(s.equals(ESList.get(i).getName()))
+        	{
+        		return ESList.get(i);
+        	};
+        }  
+	return null;
+	}
+}
